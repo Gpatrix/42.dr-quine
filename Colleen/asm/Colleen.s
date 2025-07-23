b10b07c0cf6ee6cc77bb06e1a96b07598ddb774e
Gap <132090220+Gpatrix@users.noreply.github.com>
2025-07-23 15:53:25 +0200

    [FIX|WIP]: Collen asm

--- Colleen/asm/Colleen.s
+++ Colleen/asm/Colleen.s
@@ -1,16 +1,18 @@
 global _start
 
 section .data
-    msg db "salut", 0
+    msg db "salut", 10, 0
 
 extern printf
 section .text
 
 _start:
+
     mov rdi, msg
     call printf wrt ..plt
 
 .exit:
+    add rsp, 8
     mov rax, 60
     mov rdi, 0
     syscall
