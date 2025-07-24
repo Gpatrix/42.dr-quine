4223dac77aa44c792776cdb4ec63b7d66c4826b2
Gap <132090220+Gpatrix@users.noreply.github.com>
2025-07-24 10:17:21 +0200

    [WIP]: Grace asm

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -1,49 +1,61 @@
-global main
+%define start_define global main
+%define file_name "Grace_kid.s"
+%define ZERO 0
+
+start_define
 extern dprintf
 
 section .data
-    file db "Grace_kid.s", 0
-    format db "", 0
-    O_FLAGS equ 1101o
+    file db file_name, ZERO
+    format db "test", ZERO
+
+
+%macro lets_go 1
+    st:
+        push rbx
+        mov rbp, rsp
+
+        sub rsp, 16
+
+    .open:
+        mov rax, 2
+        lea rdi, [rel file]
+        mov rsi, 0x241
+        mov rdx, 0o644
+        syscall
+
+        cmp eax, 0
+        jle .error
+
+        mov [rbp-4], eax
+        mov edi, eax
+        lea rsi, [rel format]
+        lea rdx, [rel format]
+        mov rcx, 10
+        mov r8, 34
+        xor rax, rax
+        call dprintf wrt ..plt
+
+        mov rax, 3
+        mov edi, DWORD [rbp-4]
+        syscall
+        mov rax, %1
+        jmp .return
+
+    .error:
+        mov rax, 1
+    .return:
+        add rsp, 16
+        pop rbx
+        ret ;haaaaaa
+%endmacro
 
 section .text
 
-
-
-
 main:
-push rbx
-sub rsp, 16
+    lets_go(0)
 
 
-open:
-    mov rax, 2
-    mov rdi, file
-    mov rsi, O_FLAGS
-    mov rdx, 0777
-    syscall
-
-    cmp eax, 0
-    jle .error
-
-    mov [rbp+4], eax
-    lea rsi, [rel format]
-    lea rdx, [rel format]
-    mov rcx, 10
-    mov r8, 34
-    xor rax, rax
-    call dprintf wrt ..plt
-
-    mov rax, 3
-    movzx rdi, [rbp+4]
-    syscall
-
-    xor rax, rax
-.error:
-    mov rax, -1
-.return:
-        pop rsi
-        ret ;haaaaaa
 
 ; The executable must be named Grace.
 ; •When executed, the program writes in a file named Grace_kid.c/Grace_kid.s the
