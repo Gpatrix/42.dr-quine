81b1e7e9cb5034e05524444ebe4b653c2ec7ccb5
Gap <132090220+Gpatrix@users.noreply.github.com>
2025-07-24 15:13:42 +0200

    [FIX] Sully asm: bad incr

--- Sully/ASM/Sully.s
+++ Sully/ASM/Sully.s
@@ -7,8 +7,8 @@ file_name db "./Sully_X.s", 0
 file_obj db "./Sully_X.o", 0
 file_bin  db "./Sully_X", 0
 section .rodata
-cmd db "nasm -f elf64 -o %2$s %3$s && gcc -o %1$s %2$s", 0
-format db "global main%2$cextern dprintf%2$cextern sprintf%2$cextern system%2$csection .data%2$cfile_name db %3$c./Sully_X.s%3$c, 0%2$cfile_obj db %3$c./Sully_X.o%3$c, 0%2$cfile_bin  db %3$c./Sully_X%3$c, 0%2$csection .rodata%2$ccmd db %3$cnasm -f elf64 -o %%2$s %%3$s && gcc -o %%1$s %%2$s%3$c, 0%2$cformat db %3$c%1$s%3$c, 0%2$csection .bss%2$ccmp_cmd resb 87%2$csection .text%2$cmain:%2$cpush rbx%2$cmov rbp, rsp%2$csub rsp, 16%2$cmov dl, %4$d%2$ccmp dl, byte 0%2$cje .return%2$cmov [rbp-1], dl%2$c.prepare_file_name:%2$cmov dl, byte [rbp-1]%2$cadd dl, '0'%2$clea rax, [rel file_name]%2$cmov [rax+8], dl%2$clea rax, [rel file_obj]%2$cmov [rax+8], dl%2$clea rax, [rel file_bin]%2$cmov [rax+8], dl%2$c.prepare_cmd:%2$clea rdi, [rel cmp_cmd]%2$clea rsi, [rel cmd]%2$clea rdx, [rel file_bin]%2$clea rcx, [rel file_obj]%2$clea r8, [rel file_name]%2$ccall sprintf wrt ..plt%2$c.open:%2$cmov rax, 2%2$clea rdi, [rel file_name]%2$cmov rsi, 0x241%2$cmov rdx, 0o644%2$csyscall%2$ccmp eax, 0%2$cjle .error%2$c.write:%2$cmov [rbp-5], eax%2$cmov edi, eax%2$clea rsi, [rel format]%2$clea rdx, [rel format]%2$cmov rcx, 10%2$cmov r8, 34%2$cmov r9, [rbp-1]%2$cdec r9%2$cxor rax, rax%2$ccall dprintf wrt ..plt%2$c.close:%2$cmov rax, 3%2$cmov edi, DWORD [rbp-5]%2$csyscall%2$c.comp:%2$clea rdi, [rel cmp_cmd]%2$ccall system wrt ..plt%2$c.exec:%2$clea rdi, [rel file_bin]%2$ccall system wrt ..plt%2$cxor rax, rax%2$cjmp .return%2$c.error:%2$cmov rax, 0%2$c.return:%2$cadd rsp, 16%2$cpop rbx%2$cret", 0
+cmd db "nasm -f elf64 -dQUIN=1 -o %2$s %3$s && gcc -o %1$s %2$s", 0
+format db "global main%2$cextern dprintf%2$cextern sprintf%2$cextern system%2$csection .data%2$cfile_name db %3$c./Sully_X.s%3$c, 0%2$cfile_obj db %3$c./Sully_X.o%3$c, 0%2$cfile_bin  db %3$c./Sully_X%3$c, 0%2$csection .rodata%2$ccmd db %3$cnasm -f elf64 -dQUIN=1 -o %%2$s %%3$s && gcc -o %%1$s %%2$s%3$c, 0%2$cformat db %3$c%1$s%3$c, 0%2$csection .bss%2$ccmp_cmd resb 87%2$csection .text%2$cmain:%2$cpush rbx%2$cmov rbp, rsp%2$csub rsp, 16%2$cmov dl, %4$d%2$ccmp dl, byte 0%2$cjl .return%2$c%%ifndef QUIN%2$cadd dl, 1%2$c%%endif%2$cdec dl%2$cmov [rbp-1], dl%2$c.prepare_file_name:%2$cmov dl, byte [rbp-1]%2$cadd dl, '0'%2$clea rax, [rel file_name]%2$cmov [rax+8], dl%2$clea rax, [rel file_obj]%2$cmov [rax+8], dl%2$clea rax, [rel file_bin]%2$cmov [rax+8], dl%2$c.prepare_cmd:%2$clea rdi, [rel cmp_cmd]%2$clea rsi, [rel cmd]%2$clea rdx, [rel file_bin]%2$clea rcx, [rel file_obj]%2$clea r8, [rel file_name]%2$ccall sprintf wrt ..plt%2$c.open:%2$cmov rax, 2%2$clea rdi, [rel file_name]%2$cmov rsi, 0x241%2$cmov rdx, 0o644%2$csyscall%2$ccmp eax, 0%2$cjle .error%2$c.write:%2$cmov [rbp-5], eax%2$cmov edi, eax%2$clea rsi, [rel format]%2$clea rdx, [rel format]%2$cmov rcx, 10%2$cmov r8, 34%2$cmov r9, [rbp-1]%2$cxor rax, rax%2$ccall dprintf wrt ..plt%2$c.close:%2$cmov rax, 3%2$cmov edi, DWORD [rbp-5]%2$csyscall%2$c.comp:%2$clea rdi, [rel cmp_cmd]%2$ccall system wrt ..plt%2$c.exec:%2$clea rdi, [rel file_bin]%2$ccall system wrt ..plt%2$cxor rax, rax%2$cjmp .return%2$c.error:%2$cmov rax, 0%2$c.return:%2$cadd rsp, 16%2$cpop rbx%2$cret", 0
 section .bss
 cmp_cmd resb 87
 section .text
@@ -18,7 +18,11 @@ mov rbp, rsp
 sub rsp, 16
 mov dl, 5
 cmp dl, byte 0
-je .return
+jl .return
+%ifndef QUIN
+add dl, 1
+%endif
+dec dl
 mov [rbp-1], dl
 .prepare_file_name:
 mov dl, byte [rbp-1]
@@ -52,7 +56,6 @@ lea rdx, [rel format]
 mov rcx, 10
 mov r8, 34
 mov r9, [rbp-1]
-dec r9
 xor rax, rax
 call dprintf wrt ..plt
 .close:
